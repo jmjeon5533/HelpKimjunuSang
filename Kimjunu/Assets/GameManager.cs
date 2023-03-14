d3810a1f56534d50a1608bc6354b45d3ebfa693d
jmjeon5533 <jmjeon5533@gmail.com>
2023-03-14 20:55:50 +0900

    update

--- Kimjunu/Assets/GameManager.cs
+++ Kimjunu/Assets/GameManager.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = new GameManager();
-
+    public string[,] dialog;
     [SerializeField] TextAsset StudentList; //csv
     private void Awake()
     {
@@ -14,9 +13,22 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         string[] lines = StudentList.text.Split('\n');
-        for(int i = 0; i < lines.Length ; i++)
-         {
+        for (int i = 0; i < lines.Length; i++)
+        {
             string[] columm = lines[i].Split('\t');
+            for (int j = 0; j < columm.Length; i++)
+            {
+                string text = columm[j];
+                dialog[i, j] = text;
+
+            }
+        }
+        for (int i = 0; i < dialog.GetLength(0); i++)
+        {
+            for (int j = 0; j < dialog.GetLength(1); j++)
+            {
+                print(dialog[i,j]);
+            }
         }
     }
 
